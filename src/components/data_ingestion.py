28177640921a90ff182f2c1faa2562c1409239c0
Emiliano <glabonia@mail.utdt.edu>
2024-07-23 21:14:25 -0300

    DataTransformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,11 +6,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
-    raw_data_path: str=os.path.join('artifacts','raw.csv')
-    train_data_path: str=os.path.join('artifacts','train.csv')
-    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','raw_data.csv')
+    train_data_path: str=os.path.join('artifacts','train_data.csv')
+    test_data_path: str=os.path.join('artifacts','test_data.csv')
     
 class DataIngestion:
     def __init__(self):
@@ -20,7 +22,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         
         try:
-            df = pd.read_csv('notebook\data\food.csv') #Change this line for the personal project. 
+            df = pd.read_csv('notebook/data/raw_data.csv')
             logging.info("Read the dataset as DataFrame")
             
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
@@ -42,3 +44,11 @@ class DataIngestion:
             
         except Exception as e:
             raise CustomException(e,sys)
+        
+
+if __name__ == "__main__":
+    di = DataIngestion()
+    train_data, test_data = di.initiate_data_ingestion()
+    
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,117 @@
+import sys
+import os
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path= os.path.join('artifacts','preprocessor.pkl')
+    
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+        
+    def get_data_transformer_obj(self):
+        
+        """This function is responsable for data transformation
+
+        Raises:
+            CustomException: Returns the error message details
+
+        Returns:
+            _type_: Train and Test tranformed and Preprocessing.pkl
+        """
+        
+        try:
+            numerical_features = [] #Complete with all the columns of the dataframe
+            # categorical_features=[]
+            
+            num_pipeline= Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scaler', StandardScaler())
+                ]
+            )
+            
+            logging.info('Numerical columns standard scaling is completed')
+            
+            # cat_pipeline = Pipeline(
+            #     steps=[
+            #         ('imputer',SimpleImputer(strategy='most_frequent')),
+            #         ('encoder',OneHotEncoder()),
+            #         ('scaler',StandardScaler())
+            #     ]
+            # )
+            
+            # logging.info('Categorical columns encoding completed')
+            
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_features)
+                ]
+            )
+            
+            # preprocessor=ColumnTransformer(
+            #     [
+            #         ("num_pipeline",num_pipeline,numerical_features),
+            #         ("cat_pipeline",cat_pipeline,categorical_features)
+            #     ]
+            # )
+            
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    
+    def initiate_data_transformation(self, train_path,test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            
+            logging.info("Read train and test data completed")
+            
+            logging.info("Obtaining Pre-Processing object")
+            
+            preprocessing_obj = self.get_data_transformer_obj()
+            
+            target_column_name = "Calories" #Double check
+            
+            numerical_features = [] #Complete with all the columns of the dataframe
+            
+            imput_features_train_df=train_df.drop(columns=[target_column_name], axis=1)
+            target_features_train_df = train_df[target_column_name]
+            
+            imput_features_test_df=test_df.drop(columns=[target_column_name], axis=1)
+            target_features_test_df = test_df[target_column_name]
+            
+            logging.info("Applying preprocessing object on train and test dataframe")
+            
+            imput_features_train_arr = preprocessing_obj.fit_transform(imput_features_train_df)
+            imput_features_test_arr = preprocessing_obj.transform(imput_features_test_df)
+            
+            train_arr = np.c_[imput_features_train_arr, np.array(target_features_train_df)]
+            test_arr = np.c_[imput_features_test_arr, np.array(target_features_test_df)]
+            
+            logging.info('Saved preprocessing object')
+            
+            save_object(file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                           obj=preprocessing_obj)
+            
+            return(
+                train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+if __name__ == "__main__":
+    pass
